1470af6081ffa9eda7d47bcbe8f16534e562a707
Isadora Barros Rodrigues <isadora.rodrigues70@gmail.com>
2023-01-26 11:42:53 -0300

    first commit

---
+++ quizzical/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='no-notes'>
+      <h1 className='no-notes-title'>Quizzical</h1>
+      <p className='no-notes-text'>Some description if needed</p>
+      <button className='no-notes-btn'>Start quiz</button>
+    </div>
+  )
+ 
+}
+
+export default App
